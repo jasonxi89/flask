110c85faaa58c5652eb82398acbabe4f63359d76
Xi Chen <jasonxi89@gmail.com>
2019-02-23 15:21:58 -0800

    update
    finish login part

--- app.py
+++ app.py
@@ -7,37 +7,40 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+#setting the csrf key
 app.config["SECRET_KEY"]="askjfkl132s"
-#form
+
+#form class
 class regisform(FlaskForm):
     # make sure they input something
     key = PasswordField(label=u'请输入访问密码：www.lolwaigua.com',validators=[DataRequired(u"不能为空")])
     submit = SubmitField(label=u'提交')
 
+
+#inde route
 @app.route('/index',methods=["GET","POST"])
 def index():
-    #创建对象
+    #int form class
     form = regisform()
-    error = "输入网址错误，请重新输入"
-    #判断Form 数据是否满足验证合理
+
+    #form
     if form.validate_on_submit():
         if form.key.data != "www.lolwaigua.com":
             flash(u"输入网址错误，请重新输入")
-            print("123")
             return redirect(url_for("index"))
         else:
             return redirect(url_for("func"))
 
-    return render_template("/index.html", form = form)
+    return render_template("/index.html", form = form, session = session)
 
 @app.route('/func')
 def func():
-    # stat = session.get("key","")
-    key = session.get("key", "")
-    if session.get("key", "") != "www.lolwaigua.com":
-        return redirect(url_for("func"))
-    else:
-        return "hello world"
+    # # stat = session.get("key","")
+    # key = session.get("key", "")
+    # if session.get("key") != "www.lolwaigua.com":
+    #     return redirect(url_for("func"))
+    # else:
+    return "hello world"
 
 
 
