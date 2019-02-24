9606ab2481d7e41119e379ca51d22247c45b5c87
Xi Chen <jasonxi89@gmail.com>
2019-02-24 10:55:42 -0800

    updated
    updaed

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Python-Flask Program
+2/24/2019 Update:
+
+In the beginning , i think use session is a good idea. But after learning , maybe @login_required is better solution. For the information got from form, it is similar like what you got from website when running a web crawler(of course lol), so I need a helper function to cleasn the data.
+
+
+
+
 2/23/2019 Update:
 
 Finished the Login.html, base.html and index.html with flask_bootstrap. Using csrf and wtforms and flask_wtf to make sure that the user input the right keywords to enter index.html. Also i want to use session["key"] to save a secret key so that i could verify the customer got the index.html from login.html, not just input index.html in the address.
@@ -15,5 +22,6 @@ Used Package:
 Pip3 Install Flask ,
 Pip3 Install Flask-WTF ,
 Pip3 Install bootstrap-flask ( not flask-bootstrap cause it is too old ) ,
-pip install flask-sqlalchemy ,
-pip install flask-mail
+pip3 install flask-sqlalchemy ,
+pip3 install flask-mail
+pip3 install flask-login
--- app.py
+++ app.py
@@ -1,53 +1,96 @@
 #encoding: utf-8
-from flask import Flask, redirect, url_for, render_template, session, flash
+from flask import Flask, redirect, url_for, render_template, session, flash, make_response
 from flask_wtf import FlaskForm
-from wtforms import PasswordField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms import PasswordField, SubmitField, SelectField, RadioField, FloatField
+from wtforms.validators import DataRequired, NumberRange, Required
 from flask_bootstrap import Bootstrap
+import os
+from flask_mail import Mail
+from flask_login import login_required
+
+#load supported lib part
 app = Flask(__name__)
+mail = Mail(app)
 bootstrap = Bootstrap(app)
 
+#setting email system, allow the 'less secure apps'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
 #setting the csrf key
-app.config["SECRET_KEY"]="askjfkl132s"
+app.config['SECRET_KEY']='askjfkl132s'
 
 #form class
 class regisform(FlaskForm):
+
     # make sure they input something
-    key = PasswordField(label=u'请输入访问密码：www.lolwaigua.com',validators=[DataRequired(u"不能为空")])
+    key = PasswordField(label=u'请输入访问密码：www.lolwaigua.com',validators=[DataRequired(u'不能为空')])
     submit = SubmitField(label=u'提交')
 
 
 #inde route
-@app.route('/login',methods=["GET","POST"])
+@app.route('/login',methods=['GET','POST'])
 def login():
+
     #int form class
     form = regisform()
 
     #form
     if form.validate_on_submit():
-        if form.key.data != "www.lolwaigua.com":
-            flash(u"输入访问密码错误，请重新输入")
-            return redirect(url_for("login"))
+        if form.key.data != 'www.lolwaigua.com':
+            flash(u'输入访问密码错误，请重新输入')
+            return redirect(url_for('login'))
         else:
-            session["key"]= "Toirplus"
-            return redirect(url_for("index", session = session))
+            session['key']= 'Toirplus'
+            return redirect(url_for('index', session = session))
 
-    return render_template("/login.html", form = form, session = session)
-
-
+    return render_template('/login.html', form = form, session = session)
 
 
 @app.route('/index')
+# @login_required
 def index():
-    # print(session.get("key"))
-    # using section to veri its directed from original page.
-    if session.get("key") != "Toirplus":
-        redirect(url_for("login"))
-    return render_template("/index.html")
 
-@app.route('/setting')
+    # print(session.get('key'))
+    # using section to veri its directed from original page.
+    if session.get('key') != 'Toirplus':
+        redirect(url_for('login'))
+    return render_template('/index.html')
+
+
+class jungle_form(FlaskForm):
+    Auto_Smite = RadioField(u'自动惩戒', choices=[('True','是'),('False','否')], default='True')
+    Auto_Smite_KS = RadioField(u'自动抢人头', choices=[('True','是'),('False','否')], default='True')
+    Auto_Smite_In_CombO = RadioField(u'连招使用惩戒', choices=[('True','是'),('False','否')], default='True')
+    Smite_Range = FloatField(u'惩戒使用距离', validators= [NumberRange(1,100)],default=100)
+    submit = SubmitField(label=u'提交')
+
+
+@app.route('/setting',methods=['GET','POST'])
+# @login_required
 def setting():
-    
+    form = jungle_form()
+    session = {}
+    if form.validate_on_submit():
+        for item in form:
+            print(item)
+        session['Auto_Smite'] = form.Auto_Smite.data
+        return redirect(url_for('makefile',session = session))
+    return render_template('/setting.html',form = form)
+
+
+@app.route('/makefile',methods=['GET','POST'])
+def makefile():
+    for item in session:
+        if item == 'key' or item == 'csrf_token':
+            continue
+
+        response = make_response(item + '=' + session.get(item))
+    # response.headers['Content-Disposition'] = 'attachment; filename=AutoLols.ini'
+    return "End"
 
 
 
